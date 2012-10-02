f2f3ae71e581e63dadc801d587051c0f17a5a630
Bas Wijnen <wijnen@debian.org>
2012-10-02 17:35:49 +0200

    allow missing modules; add bgloop

--- network.py
+++ network.py
@@ -21,10 +21,20 @@ import sys
 import os
 import pickle
 import socket
-import glib
-import avahi
-import dbus
-from dbus.mainloop.glib import DBusGMainLoop
+try:
+	import glib
+	have_glib = True
+	try:
+		import avahi
+		import dbus
+		from dbus.mainloop.glib import DBusGMainLoop
+		have_avahi = True
+	except:
+		have_avahi = False
+except:
+	have_avahi = False
+	have_glib = False
+	pass
 # }}}
 
 # {{{ Interface description
@@ -71,7 +81,7 @@ class Socket: # {{{
 			self.remote = address
 			self.socket = socket.socket (socket.AF_UNIX)
 			self.socket.connect (self.remote)
-		elif isinstance (address, str) and '|' in address:
+		elif have_avahi and isinstance (address, str) and '|' in address:
 			# Avahi.
 			ret = []
 			info = address.split ('|')
@@ -96,7 +106,6 @@ class Socket: # {{{
 			browser.connect_to_signal ('ItemNew', handle)
 			mainloop = glib.MainLoop ()
 			mainloop.run ()
-			print self.remote
 			self.socket = socket.create_connection (self.remote)
 		else:
 			if isinstance (address, str) and ':' in address:
@@ -104,7 +113,6 @@ class Socket: # {{{
 			else:
 				host, port = 'localhost', address
 			self.remote = (host, lookup (port))
-			print self.remote
 			self.socket = socket.create_connection (self.remote)
 	def close (self):
 		self.socket.close ()
@@ -117,81 +125,84 @@ class Socket: # {{{
 			raise EOFError ('network connection closed')
 		return ret
 	def read (self, callback, maxsize = 4096):
-		glib.io_add_watch (self.socket.fileno (), glib.IO_IN | glib.IO_PRI, lambda fd, cond: callback (self.recv (maxsize)))
+		if have_glib:
+			glib.io_add_watch (self.socket.fileno (), glib.IO_IN | glib.IO_PRI, lambda fd, cond: callback (self.recv (maxsize)))
 	def rawread (self, callback):
-		glib.io_add_watch (self.socket.fileno (), glib.IO_IN | glib.IO_PRI, lambda fd, cond: callback ())
+		if have_glib:
+			glib.io_add_watch (self.socket.fileno (), glib.IO_IN | glib.IO_PRI, lambda fd, cond: callback ())
 # }}}
 
-class Server: # {{{
-	def __init__ (self, port, datacb, acceptcb = None, address = '', backlog = 5):
-		'''Listen on a port and accept connections.'''
-		self.group = None
-		self.datacb = datacb
-		self.acceptcb = acceptcb
-		self.port = ''
-		if isinstance (port, str) and '/' in port:
-			# Unix socket.
-			self.socket = socket.socket (socket.AF_UNIX)
-			self.socket.bind (port)
-			self.port = port
-			self.socket.listen (backlog)
-		elif isinstance (port, str) and '|' in port:
-			self.socket = socket.socket ()
-			self.socket.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-			if address == '':
-				self.socket6 = socket.socket (socket.AF_INET6)
-				self.socket6.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-			info = port.split ('|')
-			self.port = port
-			if len (info) > 2:
-				self.socket.bind ((address, lookup (info[2])))
-			self.socket.listen (backlog)
-			if address == '':
-				p = self.socket.getsockname ()[1]
-				self.socket6.bind (('::1', p))
-				self.socket6.listen (backlog)
-			bus = dbus.SystemBus ()
-			server = dbus.Interface (bus.get_object (avahi.DBUS_NAME, avahi.DBUS_PATH_SERVER), avahi.DBUS_INTERFACE_SERVER)
-			self.group = dbus.Interface (bus.get_object (avahi.DBUS_NAME, server.EntryGroupNew ()), avahi.DBUS_INTERFACE_ENTRY_GROUP)
-			self.group.AddService (avahi.IF_UNSPEC, avahi.PROTO_UNSPEC, dbus.UInt32 (0), info[1], '_%s._tcp' % info[0], '', '', dbus.UInt16 (self.socket.getsockname ()[1]), '')
-			self.group.Commit ()
-		else:
-			port = lookup (port)
-			self.socket = socket.socket ()
-			self.socket.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-			self.socket.bind ((address, port))
-			self.socket.listen (backlog)
-			if address == '':
-				self.socket6 = socket.socket (socket.AF_INET6)
-				self.socket6.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-				self.socket6.bind (('::1', port))
-				self.socket6.listen (backlog)
-			self.port = port
-		fd = self.socket.fileno ()
-		glib.io_add_watch (fd, glib.IO_IN | glib.IO_PRI, self._cb)
-	def _cb (self, fd, cond):
-		new_socket = self.socket.accept ()
-		s = Socket (new_socket[0], new_socket[1])
-		if self.acceptcb is not None:
-			self.acceptcb (s)
-		if self.datacb is not None:
-			s.read (lambda data: self.datacb (s, data) or True)
-		return True
-	def close (self):
-		if self.group:
-			self.group.Reset ()
+if have_glib:
+	class Server: # {{{
+		def __init__ (self, port, datacb, acceptcb = None, address = '', backlog = 5):
+			'''Listen on a port and accept connections.'''
 			self.group = None
-		self.socket.close ()
-		self.socket = None
-		if '/' in self.port:
-			os.remove (self.port)
-		self.port = ''
-	def __del__ (self):
-		if self.socket is not None:
-			self.close ()
-# }}}
+			self.datacb = datacb
+			self.acceptcb = acceptcb
+			self.port = ''
+			if isinstance (port, str) and '/' in port:
+				# Unix socket.
+				self.socket = socket.socket (socket.AF_UNIX)
+				self.socket.bind (port)
+				self.port = port
+				self.socket.listen (backlog)
+			elif have_avahi and isinstance (port, str) and '|' in port:
+				self.socket = socket.socket ()
+				self.socket.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+				if address == '':
+					self.socket6 = socket.socket (socket.AF_INET6)
+					self.socket6.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+				info = port.split ('|')
+				self.port = port
+				if len (info) > 2:
+					self.socket.bind ((address, lookup (info[2])))
+				self.socket.listen (backlog)
+				if address == '':
+					p = self.socket.getsockname ()[1]
+					self.socket6.bind (('::1', p))
+					self.socket6.listen (backlog)
+				bus = dbus.SystemBus ()
+				server = dbus.Interface (bus.get_object (avahi.DBUS_NAME, avahi.DBUS_PATH_SERVER), avahi.DBUS_INTERFACE_SERVER)
+				self.group = dbus.Interface (bus.get_object (avahi.DBUS_NAME, server.EntryGroupNew ()), avahi.DBUS_INTERFACE_ENTRY_GROUP)
+				self.group.AddService (avahi.IF_UNSPEC, avahi.PROTO_UNSPEC, dbus.UInt32 (0), info[1], '_%s._tcp' % info[0], '', '', dbus.UInt16 (self.socket.getsockname ()[1]), '')
+				self.group.Commit ()
+			else:
+				port = lookup (port)
+				self.socket = socket.socket ()
+				self.socket.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+				self.socket.bind ((address, port))
+				self.socket.listen (backlog)
+				if address == '':
+					self.socket6 = socket.socket (socket.AF_INET6)
+					self.socket6.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+					self.socket6.bind (('::1', port))
+					self.socket6.listen (backlog)
+				self.port = port
+			fd = self.socket.fileno ()
+			glib.io_add_watch (fd, glib.IO_IN | glib.IO_PRI, self._cb)
+		def _cb (self, fd, cond):
+			new_socket = self.socket.accept ()
+			s = Socket (new_socket[0], new_socket[1])
+			if self.acceptcb is not None:
+				self.acceptcb (s)
+			if self.datacb is not None:
+				s.read (lambda data: self.datacb (s, data) or True)
+			return True
+		def close (self):
+			if self.group:
+				self.group.Reset ()
+				self.group = None
+			self.socket.close ()
+			self.socket = None
+			if '/' in self.port:
+				os.remove (self.port)
+			self.port = ''
+		def __del__ (self):
+			if self.socket is not None:
+				self.close ()
+	# }}}
 
-class RPCSocket: # {{{
+class RPCSocket (object): # {{{
 	def __init__ (self, address, object = None, disconnected = None):
 		self._object = object
 		self._disconnected = disconnected
@@ -201,7 +212,11 @@ class RPCSocket: # {{{
 			self._socket = Socket (address, None)
 		self._socket.rawread (self._cb)
 	def __getattr__ (self, attr):
-		return lambda *arg, **karg: self._call (attr, arg, karg)
+		if attr == '__methods__':
+			return self._call ('__methods__', [], {})
+		ret = lambda *arg, **karg: self._call (attr, arg, karg)
+		ret.__doc__ = self._call ('__doc__', [attr], {})
+		return ret
 	def _call (self, attr, arg, karg):
 		self._send ((attr, arg, karg))
 		while True:
@@ -236,8 +251,13 @@ class RPCSocket: # {{{
 	def _handle_recv (self, data):
 		attr, a, ka = data
 		try:
-			assert attr != '' and attr[0] != '_'
-			ret = getattr (self._object, attr) (*a, **ka)
+			if attr == '__methods__':
+				ret = [x for x in dir (self._object) if not x.startswith ('_') and callable (getattr (self._object, x))]
+			elif attr == '__doc__':
+				ret = getattr (self._object, a[0]).__doc__
+			else:
+				assert attr != '' and attr[0] != '_'
+				ret = getattr (self._object, attr) (*a, **ka)
 			self._send (('R', ret))
 		except:
 			t = sys.exc_traceback
@@ -260,4 +280,10 @@ class RPCServer: # {{{
 		rpc._object = self.factory (rpc)
 		if self.disconnected is not None:
 			rpc._disconnected = lambda: self.disconnected (rpc)
+# }}}
+
+def bgloop (): # {{{
+	if os.fork () != 0:
+		sys.exit (0)
+	glib.MainLoop ().run ()
 # }}}
